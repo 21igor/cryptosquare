eea1756bb042872c0e7e78ed9f98944f8dce25ac
Igor Seleznov and Rony Nasr <igor@>
2016-06-09 13:40:19 -0700

    added files and folders as well as backend to apply first four specs

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,25 @@
-/*This file is for your custom js.  All yours*/
+// back end logic
+var encrypt = function (text) {
+  var sentenceArray = ((text.toLowerCase()).replace(/[.,\/#!$%\^&\*;:{}=\-_`~()?’"”“ ]/g,"")).split("");
+  var size = sentenceArray.length; 
+}
+
+var rectangleRows = function(number) {
+  var sqRoot = Math.sqrt(number);
+  if (sqRoot % 1 === 0) {
+    // is square
+    return sqRoot;
+  } else {
+      return parseInt(sqRoot) + 1;
+  }
+}
+
+//Front end logic
+$(function(){
+  $("form#crypto").submit(function(event) {
+    event.preventDefault();
+    var originalText = $("input#original-text").val();
+
+    $("#result").text(encrypt(originalText));
+  });
+});
