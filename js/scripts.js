8834c14e410ef6924fb267ee2f2a920cc1b49487
Igor Seleznov and Rony Nasr <igor@>
2016-06-09 16:30:55 -0700

    add full business logic to the js

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,47 @@
 // back end logic
 var encrypt = function (text) {
-  var sentenceArray = ((text.toLowerCase()).replace(/[.,\/#!$%\^&\*;:{}=\-_`~()?’"”“ ]/g,"")).split("");
-  var size = sentenceArray.length; 
+  var sentenceArray = ((text.toLowerCase()).replace(/[.,\/#!$%\^&\*;:{}=\-_`~()?’"”“' ]/g,"")).split("");
+  var size = sentenceArray.length;
+  var matrix = splitSentence(sentenceArray,rectangle(size));
+
+  return readColumns(matrix).join("");
 }
 
-var rectangleRows = function(number) {
-  var sqRoot = Math.sqrt(number);
-  if (sqRoot % 1 === 0) {
-    // is square
-    return sqRoot;
-  } else {
-      return parseInt(sqRoot) + 1;
+var splitSentence =function (sentenceArray,dimensions) {
+  var matrix = [];
+  var j = dimensions[1];
+  for (var i=0; i < dimensions[0]; i++){ // x
+    matrix.push(sentenceArray.slice((i*j),(i*j)+j));
   }
+  return matrix;
+}
+
+var rectangle = function(number) {
+  var sqRoot = Math.sqrt(number);
+  return [Math.ceil(sqRoot),Math.floor(sqRoot)];
+}
+
+var readColumns = function(matrix){
+  var result = [];
+//  console.log(matrix[0].length);
+  for (var i=0; i < matrix.length; i++) {
+    matrix.forEach(function (row,index) {
+      //console.log(row);
+       if(row[0]){
+        result.push (row.splice(0,1)[0]);
+       }
+    });
+  }
+  result = encryptedString(result);
+  return  result;
+}
+
+var encryptedString = function (array) {
+  for (i=5; i<array.length; i=i+5) {
+  array.splice(i,0," ");
+  i++;
+  }
+  return array;
 }
 
 //Front end logic
